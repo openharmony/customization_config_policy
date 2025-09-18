165a7130fd300c516616786e880eebaa3cdd298d
sunjie <sunjie69@huawei.com>
2025-09-18 16:19:49 +0800

    config pocily 强基
    Signed-off-by: limin_321 <limin191@huawei.com>

---
+++ interfaces/ets/ani/src/ani_utils.cpp
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ani_utils.h"
+#include "hilog/log.h"
+
+#undef LOG_DOMAIN
+#define LOG_DOMAIN 0xD001E00
+
+#undef LOG_TAG
+#define LOG_TAG "ConfigPolicyJs"
+
+namespace OHOS {
+namespace Customization {
+namespace ConfigPolicy {
+static const char* CLASS_NAME_BUSINESSERROR = "L@ohos/base/BusinessError;";
+
+bool AniUtils::AniStrToString(ani_env *env, ani_string ani_str, std::string& out)
+{
+    ani_size strSize;
+    if (env->String_GetUTF8Size(ani_str, &strSize) != ANI_OK) {
+        return false;
+    }
+
+    std::vector<char> buffer(strSize + 1); // +1 for null terminator
+    char *utf8_buffer = buffer.data();
+
+    ani_size bytes_written = 0;
+    if (env->String_GetUTF8(ani_str, utf8_buffer, strSize + 1, &bytes_written) != ANI_OK) {
+        return false;
+    }
+
+    utf8_buffer[bytes_written] = '\0';
+    out = std::string(utf8_buffer);
+    return true;
+}
+
+ani_string AniUtils::StringToAniStr(ani_env *env, const std::string &str)
+{
+    ani_string result{};
+    if (ANI_OK != env->String_NewUTF8(str.c_str(), str.size(), &result)) {
+        return nullptr;
+    }
+    return result;
+}
+
+ani_ref AniUtils::CreateAniStringArray(ani_env *env, const std::vector<std::string> &paths)
+{
+    ani_ref undefinedRef = nullptr;
+    if (ANI_OK != env->GetUndefined(&undefinedRef)) {
+        HILOG_ERROR(LOG_CORE, "GetUndefined Failed.");
+    }
+
+    ani_array array;
+    if (ANI_OK != env->Array_New(paths.size(), undefinedRef, &array)) {
+        HILOG_ERROR(LOG_CORE, "new array ref error.");
+        return array;
+    }
+    for (size_t i = 0; i < paths.size(); ++i) {
+        auto item = AniUtils::StringToAniStr(env, paths[i]);
+        if (ANI_OK != env->Array_Set(array, i, item)) {
+            return array;
+        }
+    }
+    return array;
+}
+
+void AniUtils::ThrowAniError(ani_env *env, int32_t code, const std::string &message)
+{
+    ani_class cls {};
+    if (ANI_OK != env->FindClass(CLASS_NAME_BUSINESSERROR, &cls)) {
+        HILOG_ERROR(LOG_CORE, "find class %{public}s failed", CLASS_NAME_BUSINESSERROR);
+        return;
+    }
+    ani_method ctor {};
+    if (ANI_OK != env->Class_FindMethod(cls, "<ctor>", ":V", &ctor)) {
+        HILOG_ERROR(LOG_CORE, "find method BusinessError constructor failed");
+        return;
+    }
+    ani_object error {};
+    if (ANI_OK != env->Object_New(cls, ctor, &error)) {
+        HILOG_ERROR(LOG_CORE, "new object %{public}s failed", CLASS_NAME_BUSINESSERROR);
+        return;
+    }
+    if (ANI_OK != env->Object_SetPropertyByName_Int(error, "code", static_cast<ani_int>(code))) {
+        HILOG_ERROR(LOG_CORE, "set property BusinessError.code failed");
+        return;
+    }
+    ani_string messageRef {};
+    if (ANI_OK != env->String_NewUTF8(message.c_str(), message.size(), &messageRef)) {
+        HILOG_ERROR(LOG_CORE, "new message string failed");
+        return;
+    }
+    if (ANI_OK != env->Object_SetPropertyByName_Ref(error, "message", static_cast<ani_ref>(messageRef))) {
+        HILOG_ERROR(LOG_CORE, "set property BusinessError.message failed");
+        return;
+    }
+    if (ANI_OK != env->ThrowError(static_cast<ani_error>(error))) {
+        HILOG_ERROR(LOG_CORE, "throwError ani_error object failed");
+    }
+}
+} // namespace ConfigPolicy
+} // namespace Customization
+} // namespace OHOS
---
+++ interfaces/ets/ani/src/custom_config_ani.cpp
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "custom_config_ani.h"
+
+#include "application_context.h"
+#include "hilog/log.h"
+#include "hisysevent_adapter.h"
+#include "init_param.h"
+#include "ani_utils.h"
+
+#undef LOG_DOMAIN
+#define LOG_DOMAIN 0xD001E00
+
+#undef LOG_TAG
+#define LOG_TAG "CustomConfigJs"
+
+namespace OHOS {
+namespace Customization {
+namespace ConfigPolicy {
+using namespace OHOS::HiviewDFX;
+
+static const char* NAMESPACE_NAME = "L@ohos/customization/customConfig/customConfig;";
+static const std::string CHANNEL_ID_PREFIX = "const.channelid.";
+static const std::string CUSTOM_PRELOAD_LIST_PARA = "persist.custom.preload.list";
+
+ani_status CustomConfigAni::Init(ani_env* env)
+{
+    ani_namespace ns;
+    if (ANI_OK != env->FindNamespace(NAMESPACE_NAME, &ns)) {
+        HILOG_ERROR(LOG_CORE, "Not found namespace %{public}s.", NAMESPACE_NAME);
+        return ANI_ERROR;
+    }
+    std::array methods = {
+        ani_native_function { "getChannelId", nullptr, reinterpret_cast<void*>(GetChannelId) },
+    };
+    if (ANI_OK != env->Namespace_BindNativeFunctions(ns, methods.data(), methods.size())) {
+        HILOG_ERROR(LOG_CORE, "Cannot bind native methods to namespace[%{public}s]", NAMESPACE_NAME);
+        return ANI_ERROR;
+    };
+    return ANI_OK;
+}
+
+int CustomConfigAni::GetBundleName(std::string &bundleName)
+{
+    std::shared_ptr<AbilityRuntime::ApplicationContext> abilityContext =
+        AbilityRuntime::Context::GetApplicationContext();
+    if (abilityContext == nullptr) {
+        HILOG_ERROR(LOG_CORE, "get abilityContext failed.");
+        return -1;
+    }
+    bundleName = abilityContext->GetBundleName();
+    return 0;
+}
+
+bool CustomConfigAni::IsInPreloadList(std::string bundleName)
+{
+    char *preloadList = CustGetSystemParam(CUSTOM_PRELOAD_LIST_PARA.c_str());
+    if (preloadList == nullptr) {
+        HILOG_WARN(LOG_CORE, "get preload list fail.");
+        return false;
+    }
+    if (preloadList[0] < '0' || preloadList[0] > '9' || preloadList[1] != ',') {
+        HILOG_ERROR(LOG_CORE, "preload list param format error.");
+        free(preloadList);
+        return false;
+    }
+    int listlen = preloadList[0] - '0';
+    std::string preloadListResult(preloadList + 1); // skip listlen
+    free(preloadList);
+
+    for (int i = 1; i < listlen; i++) {
+        std::string tempPreloadListPara = CUSTOM_PRELOAD_LIST_PARA + std::to_string(i);
+        char *tempList = CustGetSystemParam(tempPreloadListPara.c_str());
+        if (tempList == nullptr) {
+            HILOG_ERROR(LOG_CORE, "preload list len error.");
+            return false;
+        }
+        preloadListResult.append(tempList + 1); // skip listlen
+        free(tempList);
+    }
+    preloadListResult.append(",");
+    return preloadListResult.find("," + bundleName + ",") != std::string::npos;
+}
+
+char* CustomConfigAni::CustGetSystemParam(const char *name)
+{
+    char *value = nullptr;
+    unsigned int len = 0;
+
+    if (SystemGetParameter(name, nullptr, &len) != 0 || len <= 0 || len > PARAM_CONST_VALUE_LEN_MAX) {
+        return nullptr;
+    }
+    value = (char *)calloc(len, sizeof(char));
+    if (value != nullptr && SystemGetParameter(name, value, &len) == 0 && value[0]) {
+        return value;
+    }
+    if (value != nullptr) {
+        free(value);
+    }
+    return nullptr;
+}
+
+ani_string CustomConfigAni::GetChannelId(ani_env* env)
+{
+    std::string bundleName;
+    if (GetBundleName(bundleName) != 0 || bundleName.empty() || IsInPreloadList(bundleName)) {
+        return AniUtils::StringToAniStr(env, "");
+    }
+    std::string channelKey = CHANNEL_ID_PREFIX + bundleName;
+    char *channelId = CustGetSystemParam(channelKey.c_str());
+    if (channelId == nullptr) {
+        HILOG_WARN(LOG_CORE, "get channelId failed.");
+        ReportConfigPolicyEvent(ReportType::CONFIG_POLICY_FAILED, "getChannelId", "ChannelId is nullptr.");
+        return AniUtils::StringToAniStr(env, "");
+    }
+    std::string channelIdStr(channelId);
+    ani_string result = AniUtils::StringToAniStr(env, channelIdStr);
+    free(channelId);
+    return result;
+}
+} // namespace ConfigPolicy
+} // namespace Customization
+} // namespace OHOS
+
+ANI_EXPORT ani_status ANI_Constructor(ani_vm* vm, uint32_t* result)
+{
+    ani_env* env;
+    if (ANI_OK != vm->GetEnv(ANI_VERSION_1, &env)) {
+        HILOG_ERROR(LOG_CORE, "Unsupported ANI_VERSION_1.");
+        return (ani_status)ANI_ERROR;
+    }
+
+    auto status = OHOS::Customization::ConfigPolicy::CustomConfigAni::Init(env);
+    if (status != ANI_OK) {
+        return status;
+    }
+    *result = ANI_VERSION_1;
+    return ANI_OK;
+}
